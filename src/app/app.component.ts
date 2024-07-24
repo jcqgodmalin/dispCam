11b1eecc967d02653a37f0bfead6b7a5f6d00686
Jhon Godmalin <godmalin.jc@gmail.com>
2024-07-24 18:06:33 +0800

    Update app.component.ts
    Commit from previous workstation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,52 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'dispCam';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('video') video!: ElementRef;
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+  currentFacingMode: 'user' | 'environment' = 'user'; // Default to front camera
+
+  async toggleCamera() {
+    this.currentFacingMode = (this.currentFacingMode === 'user') ? 'environment' : 'user';
+    await this.startCamera();
+  }
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.startCamera();
+  }
+
+  async startCamera() {
+    const constraints: MediaStreamConstraints = {
+      video: {
+        facingMode: { exact: this.currentFacingMode }
+      }
+    };
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      if(this.currentFacingMode === 'user') {
+        this.video.nativeElement.style.transform = 'scaleX(-1)';
+      }else{
+        this.video.nativeElement.style.transform = '';
+      }
+      this.video.nativeElement.srcObject = stream;
+      this.video.nativeElement.play(); // Start playing the video
+    } catch (error) {
+      console.error('Error accessing camera:', error);
+    }
+  }
+
+  capture(): void {
+    const videoElement = this.video.nativeElement;
+    const canvasElement = this.canvas.nativeElement;
+    const context = canvasElement.getContext('2d')!;
+
+    canvasElement.width = videoElement.videoWidth;
+    canvasElement.height = videoElement.videoHeight;
+
+    context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+    canvasElement.style.display = 'block';
+  }
 }
